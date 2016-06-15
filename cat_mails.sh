5e3c3844ef8fadbf7541d0bc4aa54e475c0f04ea
Moritz Neeb <moritz.neeb@campus.tu-berlin.de>
2016-06-15 16:58:30 +0200

    cat_mails: iterate over all 13 projects

--- cat_mails.sh
+++ cat_mails.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-files=( "gmane.comp.bug-tracking.flyspray.devel" "gmane.comp.emulators.freedos.devel" "gmane.comp.db.axion.devel" "gmane.comp.cad.geda.devel" "gmane.comp.desktop.xfce.devel.version4" "gmane.comp.boot-loaders.grub.devel" "gmane.comp.emulators.kvm.devel" )
-# "gmane.comp.boot-loaders.u-boot"
+files=( "gmane.comp.bug-tracking.flyspray.devel" "gmane.comp.emulators.freedos.devel" "gmane.comp.db.axion.devel" "gmane.comp.cad.geda.devel" "gmane.comp.desktop.xfce.devel.version4" "gmane.comp.boot-loaders.grub.devel" "gmane.comp.emulators.kvm.devel" "gmane.comp.boot-loaders.u-boot" )
 
 for i in "${files[@]}"
 do
